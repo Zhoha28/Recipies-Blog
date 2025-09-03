714615befc5f1d0bc238095f65dc4faa679a7a2b
Zhoha Damani <zhohadamani@Zhohas-MacBook-Air.local>
2025-09-02 22:26:47 -0400

    connected w contentful + setup recipie card

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,37 @@
-export default function Recipes() {
+import { createClient } from "contentful";
+import RecipieCard from "../components/RecipieCard";
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const res = await client.getEntries({ content_type: "recipie" });
+
+  return {
+    props: {
+      recipies: res.items,
+    },
+  };
+  
+}
+
+
+export default function Recipes({ recipies }) {
   return (
     <div className="recipe-list">
-      Recipe List
+
+      {recipies.map((recipe) => {
+        const { title, slug, cookingTime, thumbnail } = recipe.fields;
+
+        return (
+          <div key={recipe.sys.id} className="recipe-card">
+
+          <RecipieCard key={recipe.sys.id} recipe={recipe}/>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
