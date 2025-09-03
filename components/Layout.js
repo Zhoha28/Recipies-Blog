10675fed6d6c0b6d7f2ea6cefff96b17207db3c2
Zhoha Damani <zhohadamani@Zhohas-MacBook-Air.local>
2025-09-02 21:43:40 -0400

    base styles setup

--- components/Layout.js
+++ components/Layout.js
@@ -4,23 +4,20 @@ export default function Layout({ children }) {
   return (
     <div className="layout">
       <header>
-        <Link href="/">
-          <a>
-            <h1>
-              <span>Just Add</span>
-              <span>Marmite</span>
-            </h1>
-            <h2>Spread The Joy</h2>
-          </a>
+        <Link href="/" className="site-logo">
+          <h1>
+            <span>The</span>
+            <span>Recipe Journal</span>
+          </h1>
         </Link>
       </header>
 
-      <div className="page-content">
-        { children }
-      </div>
+      <main className="page-content">
+        {children}
+      </main>
 
       <footer>
-        <p>Copyright 2021 Just Add Marmite :)</p>
+        <p>Copyright © {new Date().getFullYear()} The Recipe Journal</p>
       </footer>
     </div>
   )
