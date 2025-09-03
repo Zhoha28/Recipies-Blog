e4780498f0d8fa19c913e5d0070691ba20bcf459
Zhoha Damani <zhohadamani@Zhohas-MacBook-Air.local>
2025-09-03 08:56:08 -0400

    final

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,66 @@
-export default function RecipeDetails() {
+import { createClient } from "contentful";
+import Image from "next/image";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
+// Contentful client
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export async function getStaticPaths() {
+  const res = await client.getEntries({ content_type: "recipie" });
+
+  const paths = res.items.map((item) => ({
+    params: { slug: item.fields.slug },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: "recipie",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: {
+      recipe: items[0],
+    },
+  };
+}
+
+export default function RecipeDetails({ recipe }) {
+  const { title, cookingTime, ingredients, method, featuredImage } = recipe.fields;
+
   return (
-    <div>
-      Recipe Details
+    <div className="recipe-detail">
+      <h2>{title}</h2>
+
+      {featuredImage?.fields?.file?.url && (
+        <Image className="featured-benner"
+          src={`https:${featuredImage.fields.file.url}`}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+          alt={title}
+        />
+      )}
+
+      <p>Takes about {cookingTime} mins to cook</p>
+
+      <h4>Ingredients</h4>
+      <ul>
+        {ingredients.map((ing) => (
+          <li key={ing}>{ing}</li>
+        ))}
+      </ul>
+
+      <h4>Method</h4>
+      <div className="method">{documentToReactComponents(method)}</div>
     </div>
-  )
+  );
 }
